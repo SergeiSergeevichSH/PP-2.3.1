476501120681dc3c167b34f73b2eefee77884db1
designruss <designruss@gmail.com>
2023-08-29 01:33:40 +0300

    Add files

---
+++ src/main/java/hiber/service/UserServiceImp.java
@@ -0,0 +1,44 @@
+package hiber.service;
+
+import hiber.dao.UserDao;
+import hiber.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class UserServiceImp implements UserService {
+    private UserDao userDao;
+
+    @Autowired
+    UserServiceImp(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @Override
+    public void addUser(User user) {
+        userDao.addUser(user);
+    }
+
+    @Override
+    public void updateUser(User user) {
+        userDao.updateUser(user);
+    }
+
+    @Override
+    public void deleteUser(long id) {
+        userDao.deleteUser(id);
+    }
+
+    @Override
+    public User getUserById(long id) {
+        return userDao.getUserById(id);
+    }
+
+    @Override
+    public List<User> listUsers() {
+        return userDao.listUsers();
+    }
+}
