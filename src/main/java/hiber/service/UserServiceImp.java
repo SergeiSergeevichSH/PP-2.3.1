131c4fa337fde66e23d37bad37f45c47bdccd59e
shish81 <74242503+shish81@users.noreply.github.com>
2023-09-04 19:13:07 +0300

    Update UserServiceImp.java

--- src/main/java/hiber/service/UserServiceImp.java
+++ src/main/java/hiber/service/UserServiceImp.java
@@ -4,8 +4,8 @@ import hiber.dao.UserDao;
 import hiber.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
@@ -18,26 +18,31 @@ public class UserServiceImp implements UserService {
     }
 
     @Override
+    @Transactional
     public void addUser(User user) {
         userDao.addUser(user);
     }
 
     @Override
+    @Transactional
     public void updateUser(User user) {
         userDao.updateUser(user);
     }
 
     @Override
+    @Transactional
     public void deleteUser(long id) {
         userDao.deleteUser(id);
     }
 
     @Override
+    @Transactional
     public User getUserById(long id) {
         return userDao.getUserById(id);
     }
 
     @Override
+    @Transactional 
     public List<User> listUsers() {
         return userDao.listUsers();
     }
