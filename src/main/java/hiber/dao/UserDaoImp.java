d28f60899ad441672536cf2b62f234fb80fc4a42
shish81 <74242503+shish81@users.noreply.github.com>
2023-09-04 17:23:13 +0300

    Update UserDaoImp.java
    @Transactional location changes

--- src/main/java/hiber/dao/UserDaoImp.java
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -1,38 +1,39 @@
 package hiber.dao;
 
 import hiber.model.User;
-import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
 import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
-@Transactional//отвечает за транзакции
 public class UserDaoImp implements UserDao {
 
     @PersistenceContext//отвечает за создание сущности @Entity
     private EntityManager entityManager;
-
+    
     @Override
+    @Transactional
     public void addUser(User user) {
         entityManager.persist(user);
     }
 
     @Override
+    @Transactional
     public User getUserById(long id) {
         return entityManager.find(User.class, id);
     }
 
     @Override
+    @Transactional
     public void updateUser(User user) {
         entityManager.merge(user);
     }
 
     @Override
+    @Transactional
     public void deleteUser(long id) {
         User user = entityManager.find(User.class, id);
         if (user != null) {
@@ -41,11 +42,11 @@ public class UserDaoImp implements UserDao {
     }
 
     @Override
+    @Transactional
     @SuppressWarnings("unchecked")
     public List<User> listUsers() {
         String jpql = "SELECT u FROM User u ORDER BY u.id";
-        return entityManager.createQuery(jpql, User.class).getResultList();
+        return entityManager.createQuery(jpql, User.class).getResultList(
 
     }
-
 }
